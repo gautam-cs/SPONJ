e121c88261e47bed3d0b48875bc11f64227be9af
rptgautam <rptgautamkr@gmail.com>
2016-11-03 12:14:53 +0530

    student register & assistant register

--- ACCOUNTS/views.py
+++ ACCOUNTS/views.py
@@ -1,8 +1,8 @@
 __author__ = 'Gautam'
 
-from ACCOUNTS.models import QuestionDetail,ProfessorDetail,CourseDetail,AssignmentDetail
-from ACCOUNTS.forms import QuestionForm,UserForm,UserProfileForm,CourseForm,AssignmentForm
-from django.conf import settings
+from ACCOUNTS.models import QuestionDetail,ProfessorDetail,CourseDetail,AssignmentDetail,AssistantDetail,StudentDetail
+from ACCOUNTS.forms import QuestionForm,UserForm,UserProfileForm,CourseForm,AssignmentForm,AssistantForm,AssistantProfileForm,\
+    StudentForm ,StudentProfileForm
 from django.shortcuts import render, get_object_or_404,render_to_response
 from django.shortcuts import redirect
 from django.http import HttpResponse
@@ -11,6 +11,7 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -40,7 +41,7 @@ def professor_login(request):
 
 def professor_logout(request):
     auth.logout(request)
-    return HttpResponseRedirect(settings.LOGIN_URL)
+    return HttpResponseRedirect('/')
 
 def professor_register(request):
     context = RequestContext(request)
@@ -48,15 +49,15 @@ def professor_register(request):
         uf = UserForm(data=request.POST, prefix='user')
         upf = UserProfileForm(data=request.POST, prefix='userprofile')
         if uf.is_valid() * upf.is_valid():
-            user=uf.save()
-            user.set_password(user.password)
-            user.is_admin = True
-            user.is_staff = True
-            user.is_superuser = False
-            user=uf.save()
+            uf=uf.save()
+            uf.set_password(uf.password)
+            uf.is_admin = True
+            uf.is_staff = True
+            uf.is_superuser = True
+            uf.save()
             userprofile=upf.save(commit=False)
             #userprofile.username(uf.username)
-            userprofile.username= user
+            userprofile.username = User.objects.all()
             userprofile.save()
             return redirect('professorlist')
     else:
@@ -64,11 +65,9 @@ def professor_register(request):
         upf = UserProfileForm(prefix='userprofile')
     return render(request,'professor\professor_register.html', {'uf':uf,'upf':upf}, context)
 
-
-
 def professorlist(request):
     professorposts = ProfessorDetail.objects.all()
-    userposts=User.objects.all()
+    profposts=User.objects.all()
     # if request.professorposts.get(username=User.objects.all()):
     # Person.objects.raw('SELECT * FROM myapp_person WHERE last_name = %s', [lname])
     """ list = zip(ProfessorDetail.objects.raw('SELECT * FROM ACCOUNTS_ProfessorDetail join ACCOUNTS_User ON'
@@ -77,9 +76,10 @@ def professorlist(request):
                     'SELECT * FROM ACCOUNTS_ProfessorDetail join User ON'
                     '(ACCOUNTS_User.username=ACCOUNTS_ProfessorDetail.username)'))"""
     # list=ProfessorDetail.objects.select_related()
-    list=zip(professorposts,userposts)
+    list=zip(professorposts,profposts)
     return render(request, 'professor\professorlist.html', {'list': list})
 
+@login_required
 def professor_home(request):
     Coursepost = CourseDetail.objects.all()
     return render(request, 'professor\professor_home.html', {'Coursepost': Coursepost})
@@ -88,8 +88,9 @@ def question(request):
     if request.method=="POST":
         form=QuestionForm(request.POST)
         if form.is_valid():
+            form = QuestionDetail(TestCaseInputFile1=request.FILES['TestCaseInputFile1'])
             form.save()
-            return redirect('qbank')
+            return redirect('questionbank')
     else:
         form=QuestionForm()
     return render(request,'professor\question.html',{'form':form})
@@ -106,12 +107,19 @@ def createcourse(request):
             return redirect('professor_home')
     else:
         courseform=CourseForm()
-    return render(request,'professor\Createcourse.html',{'courseform':courseform})
+    return render(request,'professor\createcourse.html',{'courseform':courseform})
 
 def courselist(request):
 	CoursePosts=CourseDetail.objects.all()
 	return render(request, 'professor\courselist.html',{'CoursePosts':CoursePosts})
 
+def professor_course(request):
+	return render(request, 'professor\professor_course.html')
+
+
+def specificcourse(request):
+	specific_course_post=CourseDetail.objects.all()
+	return render(request, 'professor\professor_course.html',{'specific_course_post':specific_course_post})
 
 def createassignment(request):
     if request.method=="POST":
@@ -131,61 +139,112 @@ def assignmentlist(request):
 
 
 ##############################################STUDENT IMPLEMENTATION#######################################################
+def student_register(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        sf = StudentForm(data=request.POST, prefix='student')
+        spf = StudentProfileForm(data=request.POST, prefix='studentprofile')
+        if sf.is_valid() * spf.is_valid():
+            sf=sf.save()
+            sf.set_password(sf.password)
+            sf.is_admin = True
+            sf.is_staff = True
+            sf.is_superuser = False
+            sf=sf.save()
+            studentprofile=spf.save(commit=False)
+            spf.username= sf
+            studentprofile.save()
+            return redirect('studentlist')
+    else:
+        sf=StudentForm(prefix='student')
+        spf =StudentProfileForm(prefix='studentprofile')
+    return render(request,'student\student_register.html', {'sf':sf,'spf':spf}, context)
+
+def studentlist(request):
+    studentposts = StudentDetail.objects.all()
+    stposts=User.objects.all()
+    list=zip(studentposts,stposts)
+    return render(request, 'student\studentlist.html', {'list': list})
+
 def student_login(request):
     context = RequestContext(request)
-    user = User.objects.all()
+    user=User.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-    if user is not None:
-        return HttpResponse("Making is in progress ")
+        #print("auth",str(authenticate(username=username, password=password)))
 
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('studentlist')
+            else:
+                return HttpResponse("already login")
+        else:
+            #print ("please choose your valid username & password: {0}, {1}".format(username, password))
+            return HttpResponse("please choose your valid username & password ")
     else:
         return render_to_response('student/student_login.html', {}, context)
 
-def student_logout(request):
+def professor_logout(request):
     auth.logout(request)
-    return HttpResponseRedirect(settings.LOGIN_URL)
-
-def student_home(request):
-    return render (request, "student/student_home.html")
-
-def student_register(request):
-    context = RequestContext(request)
-    if request.method == 'POST':
-        return HttpResponse("Student register page Making is in progress ")
-    else:
-        uf=UserForm(prefix='user')
-        upf = UserProfileForm(prefix='userprofile')
-    return HttpResponse("Student register page Making is in progress ")
+    return HttpResponseRedirect('/')
 #############################################################################################################################
 
 
 ##############################################ASSISTANT IMPLEMENTATION#######################################################
+def assistant_register(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        af = AssistantForm(data=request.POST, prefix='assistant')
+        apf = AssistantProfileForm(data=request.POST, prefix='assistantprofile')
+        if af.is_valid() * apf.is_valid():
+            af=af.save()
+            af.set_password(af.password)
+            af.is_admin = True
+            af.is_staff = True
+            af.is_superuser = False
+            af=af.save()
+            assistantprofile=apf.save(commit=False)
+            apf.username= af
+            assistantprofile.save()
+            return redirect("assistantlist")
+    else:
+        af=AssistantForm(prefix='assistant')
+        apf =AssistantProfileForm(prefix='assistantprofile')
+    return render(request,'assistant\Assistant_register.html', {'af':af,'apf':apf}, context)
+
+def assistantlist(request):
+    assistantposts = AssistantDetail.objects.all()
+    asposts=User.objects.all()
+    list=zip(assistantposts,asposts)
+    return render(request, 'assistant\Assistantlist.html', {'list': list})
+
 def assistant_login(request):
     context = RequestContext(request)
-    user = User.objects.all()
+    user=User.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
+        #print("auth",str(authenticate(username=username, password=password)))
 
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('assistantlist')
+            else:
+                return HttpResponse("already login")
+        else:
+            #print ("please choose your valid username & password: {0}, {1}".format(username, password))
+            return HttpResponse("please choose your valid username & password ")
     else:
         return render_to_response('assistant/assistant_login.html', {}, context)
 
-
-def assistant_logout(request):
+def professor_logout(request):
     auth.logout(request)
-    return HttpResponseRedirect(settings.LOGIN_URL)
+    return HttpResponseRedirect('/')
 
-def assistant_register(request):
-    context = RequestContext(request)
-    if request.method == 'POST':
-        return HttpResponse("Assistant register page Making is in progress ")
-    else:
-        uf = UserForm(prefix='user')
-        upf = UserProfileForm(prefix='userprofile')
-    return HttpResponse("Assistant register page Making is in progress ")
 ############################################################################################################################
 
