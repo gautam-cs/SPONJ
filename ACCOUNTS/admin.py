77c404a730ec3fb7e91c1e42dfe79b8997d8eb0b
rptgautam <rptgautamkr@gmail.com>
2016-11-12 19:38:00 +0530

    changes in Admin

--- ACCOUNTS/admin.py
+++ ACCOUNTS/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from .models import QuestionDetail,ProfessorDetail,AssignmentDetail,CourseDetail\
-    ,AssistantDetail,Assignment_languages,StudentDetail,Submission,Course_student,Courses_Ta
+    ,AssistantDetail,Assignment_languages,StudentDetail,Submission,Course_student,Courses_Ta,AssignmentQuestion
 
 
 admin.site.register(QuestionDetail)
@@ -14,3 +14,4 @@ admin.site.register(Course_student)
 admin.site.register(Submission)
 admin.site.register(StudentDetail)
 admin.site.register(Assignment_languages)
+admin.site.register(AssignmentQuestion)
